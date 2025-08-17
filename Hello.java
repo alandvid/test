cf0eef4ce0ef1296006c8776a196cb3bcab3b6ae
alandvid <alandvid2004@gmail.com>
2025-08-17 20:43:00 +0300

    first version

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
-class Hello_class{
+class Hello{
 		public static void main(String[] args) {
-					for (int i=1;i<=12; i++) {
+					for (int i=1;i<=10; i++) {
 									System.out.println("Hello World ..."+i);
 											}
 						}
-}
